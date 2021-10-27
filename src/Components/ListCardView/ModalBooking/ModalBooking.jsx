079b09a9a92bce1ffc2f569a1a14f7582bde980e
azacoder <azamatamerika2@gmail.com>
2021-10-27 12:39:05 +0500

    bag-fix-modalbooking

--- src/Components/ListCardView/ModalBooking/ModalBooking.jsx
+++ src/Components/ListCardView/ModalBooking/ModalBooking.jsx
@@ -1,102 +1,31 @@
-import './ListcardView.css'
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom'
-import DatePicker from "react-datepicker";
-import { Button} from 'react-bootstrap';
-import "react-datepicker/dist/react-datepicker.css";
-import { ModalBooking } from './ModalBooking/ModalBooking';
+import { Modal, Button } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export const ListCardView = () => {
-    
-        
-    const [homeListings, setHomeListings] =  useState([]);
-    const [startDate, setStartDate] = useState(null); 
-    const [endDate, setEndDate] = useState(null); 
-    const [modalShow, setModalShow] = useState(false);
-    let startDay = Date.parse(startDate); 
-    let endDay = Date.parse(endDate); 
-    let milseconds = 86400000; 
-    let rentalDays = (endDay - startDay + milseconds) /milseconds; 
-    
 
-    
-      const fetchData = () => {
-        fetch('/fakeHost/data.json').then(response => {
-            return response.json()
-        }).then(({data}) => {
-          setHomeListings(data)
-        });
-      }
-    
-      useEffect(() => {
-        fetchData()
-        
-        
-     }, []);
-    
-    const {id} = useParams(); 
 
-    let CardFilter = homeListings.filter((el)=>{
-        return el.id == id
-        
-    })
-    return CardFilter.map((el)=>{
-      return(
-        <div className="title">
-            <div className="adpage">
-               <div className="cardbox">
-               <div><img src={el.image} alt="photo" /></div>
-                <div><span>{el.city}</span><span>{el.adress}</span></div>
-                <p>{el.title}</p>
-                <hr />
-                <p>User Name</p>
-                <hr />
-                <p>About this space</p>
-                <div>
-                  <button>{el.hometype}</button>
-                  <button>{el.guests}</button>
-                </div>
-                <div>{el.description}</div>
-
-               </div>
-
-                <div className="bookingbox">
-                  <div className="bookpadding">
-                  <p className="price">${el.price}/day</p>
-                  <hr />
-                  <p>Check In</p>
-                  <DatePicker 
-                      selected={startDate} 
-                      onChange={(date)=>{setStartDate(date)}}
-                      dateFormat='yyyy/MM/dd'
-                      minDate={new Date} 
-                      maxDate={endDate}
-                      placeholderText="Select date"/> 
-                      
-                  <p>Check Out</p>
-                  <DatePicker 
-                      selected={endDate} 
-                      onChange={(date)=>{setEndDate(date)}}
-                      dateFormat='yyyy/MM/dd'
-                      disabled ={startDate === null ? true : false}
-                      minDate={startDate}
-                      placeholderText="Select date"/> 
-                  <hr />
-                  <Button className ={endDate === null || startDate === null ? "btn disabled btn-secondary btn-mg ": "btn active btn-primary btn-mg"} 
-                          onClick={() => setModalShow(endDate === null || startDate === null ? false : true)} 
-                          >Request to book</Button>
-                  <p>You won't be charged yet</p>
-                  </div>
-                </div>
-                <ModalBooking rentalDays = {rentalDays} price={el.price} show={modalShow} onHide={() => setModalShow(false)}/>
-            </div>
-        </div>
-      )
-    })
-  
-   
-
-  
-
-    
-}
+export function ModalBooking (props) {
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header >
+          <Modal.Title id="contained-modal-title-vcenter">
+            Modal heading
+            <button onClick={props.onHide} type="button" class="btn-close" aria-label="Close"></button>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Book your trip</h4>
+          <p>
+            ${props.price} * {props.rentalDays} days = ${props.price*props.rentalDays}
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Book</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
