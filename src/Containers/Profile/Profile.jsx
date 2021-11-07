2c0bc465c0476d5ddfd5418bbdd729fed7f6c843
AlinaDzhanbulat <a.janbolotovna@gmail.com>
2021-11-07 20:29:38 +0300

    change profile and add modal

--- src/Containers/Profile/Profile.jsx
+++ src/Containers/Profile/Profile.jsx
@@ -1,33 +1,55 @@
-// import React from 'react'
-// import './Profile.css';
-// import { Button, Card, Col, Image } from 'react-bootstrap'
+import React, { useEffect } from "react";
+import "./Profile.css";
+import { Button, Card, Image } from "react-bootstrap";
+import { useState } from "react";
+import { MyVerticallyCenteredModal } from "./modal-for-prof/profileModal";
 
-// const Profile = () => {
-//     return (
-//         <>
-//             <Card bg="light" style={{ margin: '0 auto', marginTop: '50px', width: '380px' }}>
-//                 <Image style={{ width: '90px', height: '60px', marginLeft: '140px', marginTop: '40px' }} src="https://www.easytutorial.com/wp-content/uploads/2020/03/create-user-account-admin-windows-10-easy-tutorial-how-to-340x160.png" roundedCircle />
-//                 <Card.Body className='bo'>
-//                     <Card.Title className='theme'>Details</Card.Title>
-//                     <Card.Text className='acc'>
-//                         <p className='na'>Name: <b>Bilal Akbalaev</b> <br /> </p>
-//                         <p className='ac'>Contact: <b>bilu@gmail.com</b></p>
-//                     </Card.Text>
-//                     <hr />
-//                     <Card.Title className='theme'>Additional Details</Card.Title>
-//                     <Card.Text>
-//                         Interested in becoming a TinyHouse host? Register with your Stripe account!
-//                     </Card.Text>
-//                     <Button className='str' variant="primary">Connect with Stripe</Button>
-//                     <Card.Text className='lt'>
-//                         TinyHouse uses <a style={{ textDecoration: 'none' }} href="https://stripe.com/connect">Stripe</a> to help transfer your earnings in a secure and truster manner.
-//                     </Card.Text>
-//                 </Card.Body>
-//             </Card>
-//         </>
-//     )
-// }
+const Profile = () => {
+  const [modalShow, setModalShow] = React.useState(false);
+  return (
+    <>
+      <Card className="card-profile">
+        <Image
+          className="image-profile"
+          src="https://www.easytutorial.com/wp-content/uploads/2020/03/create-user-account-admin-windows-10-easy-tutorial-how-to-340x160.png"
+          roundedCircle
+        />
+        <Card.Body className="bo">
+          <Card.Title className="theme">Details</Card.Title>
+          <Card.Text className="acc">
+            <p className="na">
+              Name: <b>Bilal Akbalaev</b> <br />{" "}
+            </p>
+            <p className="ac">
+              Contact: <b>bilu@gmail.com</b>
+            </p>
+          </Card.Text>
+          <hr />
+          <Card.Title className="theme">Additional Details</Card.Title>
+          <Card.Text className="ltt">
+            If you want to place an ad that you are renting an apartment or room
+            for daily rent, you will have to pay for it.
+          </Card.Text>
+          <Button variant="primary" onClick={() => setModalShow(true)}>
+            Add payment
+          </Button>
+          <MyVerticallyCenteredModal
+            show={modalShow}
+            onHide={() => setModalShow(false)}
+          />
+          <Card.Text className="lt">
+            TinyHouse uses{" "}
+            <a className="a-profile" href="https://stripe.com/connect">
+              Stripe
+            </a>{" "}
+            to help transfer your earnings in a secure and truster manner.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
 
-// export default Profile;
+export default Profile;
 
 
