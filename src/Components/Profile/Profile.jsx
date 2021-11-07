caedbb30c7389893c1e8a6d54e0f86d28ff3e2d7
Bilal <akbalaevbilal8@gmail.com>
2021-11-07 21:08:23 +0600

    ch

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col, Image } from "react-bootstrap";
+import { Button, Col, Dropdown, Image, OverlayTrigger, Tooltip } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import "./Profile.css";
 
@@ -7,12 +7,40 @@ const Profile = (props) => {
   const dataUser = useSelector((state) => state.userData);
   console.log(dataUser);
   return (
-    <div>
-      <Col>
-        <Image className="avaImg" src={dataUser.avatar} roundedCircle />
-      </Col>
-    </div>
-  );
+    // <div>
+    //   <Col>
+    //     <Image className="avaImg" src={dataUser.avatar} roundedCircle />
+    //   </Col>
+    // </div>
+    // <OverlayTrigger
+    //   placement="auto"
+    //   overlay={<Tooltip id="button-tooltip-2">  <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+    //     <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+    //     <Dropdown.Item href="#/action-3">Something else</Dropdown.Item></Tooltip>}
+    // >
+    //   {({ /* ref, */ ...triggerHandler }) => (
+    //     <Button
+    //       // variant="light"
+    //       {...triggerHandler}
+    //     // className="d-inline-flex align-items-center"
+    //     >
+    //       
+    //     </Button>
+    //   )}
+    // </OverlayTrigger>
+
+    <Dropdown>
+    <Dropdown.Toggle variant='light'>
+    <Image className="avaImg" src={dataUser.avatar} roundedCircle />
+    </Dropdown.Toggle>
+    
+    <Dropdown.Menu>
+      <Dropdown.Item href="#/action-1">Profile</Dropdown.Item>
+      <Dropdown.Item href="#/action-2">Log out</Dropdown.Item>
+    </Dropdown.Menu>
+    </Dropdown>
+  )
+
 };
 
 export default Profile;
